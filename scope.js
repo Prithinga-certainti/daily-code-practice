2edf6e22d19bb39d7592772a83f8e81b2d1cb1eb
User <user@example.com>
2026-03-18 17:21:27 +0530

    Day 4

---
+++ scope.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let totalRequest = 0;
+let students = [];
+
+app.get('/students', (req, res) => {
+    totalRequest++;
+    let message = "Students fetched";
+    res.json({
+        message,
+        totalRequest,
+        students
+    });
+});
+
+app.post('/students', (req, res) => {
+    totalRequest++;
+    const { name, age, year } = req.body;
+    if (!name || !age || !year) {
+        let error = "Invalid input"; 
+        return res.status(400).json({ error });
+    }
+    let newStudent = { name, age, year };
+    students.push(newStudent);
+    res.json({
+        message: "Student added successfully",
+        student: newStudent
+    });
+});
+
+
+app.get('/status', (req, res) => {
+    totalRequest++;
+    if (totalRequest > 5) {
+        let status = "High traffic detected"; 
+        res.json({ status });
+    } else {
+        let status = "Traffic is normal";
+        res.json({ status });
+    }
+});
+
+app.get('/stats', (req, res) => {
+    res.json({ totalRequest });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
