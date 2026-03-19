b90d6dd8469dba48332eb6a70f5c232ff145e6de
User <user@example.com>
2026-03-19 15:54:51 +0530

    Update Day5.js

--- Day5.js
+++ Day5.js
@@ -8,18 +8,19 @@ let user={
     id: 12345,
     age:20
 }
-app.get('user',(req,res)=>{
+app.get('/user',(req,res)=>{
     res.json({
         message :"Available user data",
         data:user
     });
 })
-app.get('/user/:id',(req,res)=>{
-    const{id}=req.params;
-    if(id != user.id){
+app.get('/user/:name',(req,res)=>{
+    const{name}=req.params;
+    if(user.name !== name){
         console.log("User not found");
-        return res.status(404).json({message:"User not found"});    
+        return res.status(404).json({message:"User not found"});
     }
+        user['name'] = name;
     res.json({
         message : "User data fetched successfully",
         data:user
@@ -28,19 +29,18 @@ app.get('/user/:id',(req,res)=>{
 
 app.post('/user',(req,res)=>{
     const {name,id,age} = req.body;
-    const user={
-        name,
-        id,
-        age
-    };
+    user.name = name;
+    user.id = id;
+    user.age = age;
     res.json({
         message: "User created successfully",
         data: user
     });
 });
 
-app.get('/user',(req,res)=>{
-    user['dept'] = "Computer Science";  
+app.put('/user',(req,res)=>{
+    const {dept} = req.body;
+    user['dept'] = dept;  
     res.json({
         message : "All users data fetched successfully",
         data:user
@@ -49,7 +49,7 @@ app.get('/user',(req,res)=>{
 
 app.delete('user/:id',(req,res)=>{
     const{id} = req.params;
-    if(id!=user.id){
+    if(id!==user.id){
         console.log("User not found");
         return res.status(404).json({message:"User not found"});
     }
